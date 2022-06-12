a28fd425936d0153f4de8b65d8905f62661075d7
Akhil-gadde <90595069+Akhil-gadde@users.noreply.github.com>
2022-06-13 01:25:27 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+#add implementation
+def add(x,y):
+	return x+y
+
+#add implementation
+def subtract(x,y):
+	pass
+
+#add implementation
+def multiply(x,y):
+	pass
+
+#add implementation
+def divide(x,y):
+	pass
+
